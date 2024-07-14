811397b58a6b6c297d24e3fff118d8938aa3ebb6
luizleal <luizandreleal100@gmail.com>
2024-07-14 20:50:51 -0300

    Feat: Show Dialog when import from url button is clicked

--- app/src/main/java/dev/luizleal/markdowneditor/ui/fragments/HomeFragment.kt
+++ app/src/main/java/dev/luizleal/markdowneditor/ui/fragments/HomeFragment.kt
@@ -1,19 +1,23 @@
 package dev.luizleal.markdowneditor.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.leinardi.android.speeddial.SpeedDialActionItem
 import dev.luizleal.markdowneditor.R
 import dev.luizleal.markdowneditor.databinding.FragmentHomeBinding
+import dev.luizleal.markdowneditor.ui.view.MainActivity
+import dev.luizleal.markdowneditor.ui.viewmodel.NoteViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +28,30 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as MainActivity).viewModel
+    }
+
     override fun onStart() {
         super.onStart()
 
-        setSpeedViewItem(getString(R.string.import_from_file), R.id.fab_import_from_file, R.drawable.ic_upload_file)
-        setSpeedViewItem(getString(R.string.import_from_url), R.id.fab_import_from_link, R.drawable.ic_add_link)
-        setSpeedViewItem(getString(R.string.new_markdown), R.id.fab_write_new, R.drawable.ic_text_increase)
+        setSpeedViewItem(
+            getString(R.string.import_from_file),
+            R.id.fab_import_from_file,
+            R.drawable.ic_upload_file
+        )
+        setSpeedViewItem(
+            getString(R.string.import_from_url),
+            R.id.fab_import_from_link,
+            R.drawable.ic_add_link
+        )
+        setSpeedViewItem(
+            getString(R.string.new_markdown),
+            R.id.fab_write_new,
+            R.drawable.ic_text_increase
+        )
 
         setupSpeedViewItemClicked()
     }
@@ -37,7 +59,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun setSpeedViewItem(label: String, idRef: Int, iconRef: Int) {
         binding.speedviewNew.addActionItem(
             SpeedDialActionItem.Builder(idRef, iconRef)
-                .setFabBackgroundColor(ContextCompat.getColor(requireContext(), R.color.lightPrimary))
+                .setFabBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.lightPrimary
+                    )
+                )
                 .setFabImageTintColor(ContextCompat.getColor(requireContext(), R.color.white))
                 .setLabel(label)
                 .create()
@@ -51,14 +78,29 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                     this.findNavController().navigate(R.id.action_homeFragment_to_editorFragment)
                     true
                 }
-                else -> false
-//                R.id.fab_import_from_link -> {
-//
-//                }
+
+                R.id.fab_import_from_link -> {
+                    showAlertDialogForImport()
+                    true
+                }
 //                R.id.fab_import_from_file -> {
 //
 //                }
+                else -> false
             }
         }
+    }
+
+    private fun showAlertDialogForImport() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setView(R.layout.dialog_import_from_url)
+            .setPositiveButton(getString(R.string.positive_button)) { _, _ ->
+
+            }
+            .setNegativeButton(getString(R.string.negative_button)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create().show()
+
     }
 }
