a680dada3d130b1ecf5b3e0d5957719be00783e8
luizleal <luizandreleal100@gmail.com>
2024-07-15 13:47:16 -0300

    Feat: Navgraph arguments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,12 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
+buildscript {
+    repositories {
+        google()
+    }
+
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
