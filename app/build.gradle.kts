a6b197e266023032b73090d167cea75b790941f9
luizleal <luizandreleal100@gmail.com>
2024-07-16 11:21:46 -0300

    Fix: Parcelize annotation added in note model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.devtools.ksp")
     id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-parcelize")
 }
 
 android {
--- app/src/main/java/dev/luizleal/markdowneditor/model/Note.kt
+++ app/src/main/java/dev/luizleal/markdowneditor/model/Note.kt
@@ -1,14 +1,17 @@
 package dev.luizleal.markdowneditor.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import dev.luizleal.markdowneditor.constants.DatabaseConstants
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = DatabaseConstants.NOTE_TABLE)
+@Parcelize
 data class Note(
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
     val text: String,
     @ColumnInfo(name = "last_update_day") val lastUpdateDay: Int,
     @ColumnInfo(name = "last_update_month") val lastUpdateMonth: Int
-)
+) : Parcelable
