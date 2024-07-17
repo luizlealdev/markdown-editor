45c57b7f66ea6525047a4dc50a38235751407d73
luizleal <luizandreleal100@gmail.com>
2024-07-17 11:04:36 -0300

    Feat: More action button function added

--- app/src/main/java/dev/luizleal/markdowneditor/ui/adapter/NoteListAdapter.kt
+++ app/src/main/java/dev/luizleal/markdowneditor/ui/adapter/NoteListAdapter.kt
@@ -1,14 +1,17 @@
 package dev.luizleal.markdowneditor.ui.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import dev.luizleal.markdowneditor.databinding.NoteItemHolderBinding
 import dev.luizleal.markdowneditor.model.Note
 import dev.luizleal.markdowneditor.utils.CalendarUtils.Companion.getMouthName
 
-class NoteListAdapter(private val onItemClicked: (Note) -> Unit): RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
+class NoteListAdapter(
+    private val onItemClicked: (Note) -> Unit,
+    private val onMoreActionsButtonClicked: (Note, parent: View) -> Unit
+) : RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
     private var items: List<Note> = ArrayList()
 
     fun setItems(items: List<Note>) {
@@ -26,7 +29,7 @@ class NoteListAdapter(private val onItemClicked: (Note) -> Unit): RecyclerView.A
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         when (holder) {
             is NoteViewHolder -> {
-                holder.bind(items[position], onItemClicked)
+                holder.bind(items[position], onItemClicked, onMoreActionsButtonClicked)
             }
         }
     }
@@ -35,19 +38,28 @@ class NoteListAdapter(private val onItemClicked: (Note) -> Unit): RecyclerView.A
         return items.size
     }
 
-    class NoteViewHolder(private var binding: NoteItemHolderBinding): RecyclerView.ViewHolder(binding.root) {
+    class NoteViewHolder(private var binding: NoteItemHolderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         private var title = binding.textTitle
         private var date = binding.textDate
 
-        fun bind(note: Note, onItemClicked: (Note) -> Unit) {
+        fun bind(
+            note: Note,
+            onItemClicked: (Note) -> Unit,
+            onMoreActionsButtonClicked: (Note, parent: View) -> Unit
+        ) {
             title.text = note.text.lineSequence().first()
 
             val mouthName = getMouthName(note.lastUpdateMonth)
-            val formatedDate =  "${note.lastUpdateDay} $mouthName"
+            val formatedDate = "${note.lastUpdateDay} $mouthName"
             date.text = formatedDate
 
             binding.root.setOnClickListener {
                 onItemClicked(note)
+            }
+
+            binding.buttonNoteMoreActions.setOnClickListener {
+                onMoreActionsButtonClicked(note, it)
             }
         }
     }
